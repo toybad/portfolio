f61516c340862e6060f312f431b62e5839a45bcb
Badmus Toyeeb <toygud14@gmail.com>
2021-04-12 14:40:48 +0100

    added some stylings

--- app.js
+++ app.js
@@ -8,3 +8,101 @@ if (a > c) {
 else {
     print "You are not wecome";
 }
+
+
+
+ul {
+    list-style-type:none;
+    margin: 0;
+    padding: 20;
+    overflow:visible;
+    width: 100%;
+    text-align: right;
+    position: fixed;
+    background-color: rgb(251, 212, 212);
+    float: right;
+
+  }
+  
+  li {
+    float: right;
+    border-right: 1px solid #bbb;
+    background-color: violet;
+    color: #111;
+
+  }
+  
+  li a {
+    display:flex;
+    flex-direction: column;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+  
+  li a:hover {
+    background-color: #111;
+  }
+
+  .myself{
+    text-align: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: relative;
+    padding-top: 10px;
+    padding-bottom: 60px;
+    height: 200px;
+
+}
+.portfo{
+    text-align: center;
+    background-color: rosybrown;
+    padding: 10px 0px 10px 0px;
+}
+.sample_work{
+    align-items: center;
+    text-align: center;
+}
+.about{
+    text-align: center;
+    background: darkblue;
+    padding: 100px 20px 100px 20px;
+    color: white;
+}
+.contact{
+    text-align: center;
+    background: #D24D57;
+    padding: 100px 20px 100px 20px;
+    color: whitesmoke;
+    margin-top: -40px;
+}
+.behance{
+    box-shadow: salmon;
+}
+.btn{
+  background-color: transparent;
+  height: 30px;
+  border: 3px;
+  border-radius: 2px;
+  border-color: white;
+}
+.btn a:hover{
+  color: white;
+  background-color: tomato;
+}
+.u{
+  max-width: 10px;
+}
+.btn2{
+  height: 50px; 
+  background: transparent;
+  border: whitesmoke; 
+  border-style: double; 
+  width: 200px;
+  color: whitesmoke;
+}
+.btn2 a:hover{
+  color: transparent;
+  background-color: whitesmoke;
+}
